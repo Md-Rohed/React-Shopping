cfc62bf56a5c49efae44b24b2baf1ff6d74bae1e
Md-Rohed <mdrohed85@gmail.com>
2021-11-17 00:04:51 +0600

    update course_data

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,17 +4,19 @@ import "./Cart.css";
 
 export default function Cart(props) {
   const cart = props.cart;
-  console.log(cart);
+  console.log(cart.img);
 
   const CoursePrice = cart.reduce((sum, course) => sum + course.price, 0);
   const NewCoursePrice = cart.reduce((sum, course) => course.price, 0);
   const tax = CoursePrice / 10;
+  const CourseImg = cart.reduce((sum, course) => course.img, 0);
 
   return (
     <div>
       <h1> Total item ordered:{cart.length}</h1>
       <h1> Tax:{tax}</h1>
       <h1>Course price:{NewCoursePrice}</h1>
+      <h1> {CourseImg}</h1>
 
       <h1> Total price is :{CoursePrice + tax}</h1>
       <button className="btn">Place order</button>
